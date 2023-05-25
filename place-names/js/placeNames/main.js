eee44c3b8fe9d1fab7848bb16593724c8da428f1
Austin Davis <davis.austin1.1.c@gmail.com>
2023-05-24 20:16:24 -0400

    Completed Place Names Translation

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,12 @@
 // Put your code here
 
-const names = [
+const locationNames = [
    "Nashville", "Hong Kong", "The back yard", "Earth",
     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
 console.log("All Pace Names");
 
-for (const name of names) {
+for (const name of locationNames) {
   console.log(name);
 }
